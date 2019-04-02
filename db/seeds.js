cfa4d62e2e39b68c36f6eaa0ec9677e5f706803d
ASH Walls <admin@arethawalls.com>
2019-04-02 15:00:42 -0700

    Starting views work, fixing image url

--- db/seeds.js
+++ db/seeds.js
@@ -36,7 +36,7 @@ db.once('open', function () {
     })
     const scandalInBohemia = new Book({
         title: 'A Scandal in Bohemia',
-        cover: 'assets/images/bohemia-cover.jpg',
+        cover: 'images/bohemia-cover.jpg',
         authors: [doyle],
         chapters: [chapter1, chapter2],
     });
