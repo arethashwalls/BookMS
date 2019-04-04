c4c2fff022b6e497c2b0a3571046fc563eb8a12a
ASH Walls <admin@arethawalls.com>
2019-04-03 19:51:03 -0700

    Setup jwt signer

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,8 +8,17 @@ module.exports = {
             title: 'A Scandal in Bohemia'
         })
     },
-    postLoginAuth: passport.authenticate('login', { session: false }),
-    postLoginRedir: (req, res) => {
-        res.redirect('/a-scandal-in-bohemia');
+    postLoginAuth: passport.authenticate('login', { session: false } ),
+    postLoginRedir: (req, res, next) => {
+        const { user } = req;
+        req.login(user, { session: false }, err => {
+            if(err) return next(err);
+            const body = {
+                _id: user._id,
+                email: user.email
+            }
+            const token = jwt.sign({user: body}, "It's a secret!")
+        // res.redirect('/a-scandal-in-bohemia');
+        });
     }
 }
