e73ab41c76f30981f7c9135cf65d08ad94ff9e1b
ASH Walls <admin@arethawalls.com>
2019-04-04 12:02:44 -0700

    Commenting

--- auth/auth.js
+++ auth/auth.js
@@ -1,24 +1,32 @@
+//Imports:
 const passport = require('passport'),
       { Strategy: LocalStrategy } = require('passport-local'),
       { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt'),
       { User } = require('../models');
 
+//Define register strategy:
 passport.use('register', new LocalStrategy({
+    //Name fields appropriately:
     usernameField: 'email',
     passwordField: 'password'
 }, (email, password, next) => {
+    //Attempt to create new user:
     User.create({ email, password })
     .then(user => next(null, user))
     .catch(err => next(err))
 }));
 
+//Define login strategy:
 passport.use('login', new LocalStrategy({
+    //Name fields appropriately:
     usernameField: 'email',
     passwordField: 'password'
 }, (email, password, next) => {
+    //Attempt to find matching user:
     User.findOne({ email })
     .then(user => {
         if(!user) return next(null, false, { message : 'User not found.'});
+        //Validate password:
         user.isValidPassword(password)
         .then(validate => {
             if(!validate) {
@@ -32,7 +40,9 @@ passport.use('login', new LocalStrategy({
     .catch(err => next(err))
 }));
 
+//Define JWT strategy:
 passport.use(new JWTStrategy({
+    //Get secret key from .env:
     secretOrKey: process.env.AUTH_SECRET,
     jwtFromRequest: ExtractJwt.fromUrlQueryParameter('secret_token')
 }, (token, next) => {
