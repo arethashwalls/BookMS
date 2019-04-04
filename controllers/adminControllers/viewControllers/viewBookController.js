c5cd51acad8f803d59749b91cd830cbc7cbc2dee
ASH Walls <admin@arethawalls.com>
2019-04-04 15:06:49 -0700

    Can pull data from API to view books

--- controllers/adminControllers/viewControllers/viewBookController.js
+++ controllers/adminControllers/viewControllers/viewBookController.js
@@ -2,19 +2,15 @@ const axios = require('axios');
 
 module.exports = {
     getViewBooks: (req, res) => {
-
-        // res.render('admin/viewers/viewBooks', {
-        //     username: 'Arthur',
-        //     num: 3,
-        //     title: 'AL:KFHJWJH',
-        //     books: [
-        //         {title: 'AH'},
-        //         {title: 'lbkwhj'},
-        //         {title: 'shazam!'},
-        //         {title: 'AH'},
-        //         {title: 'lbkwhj'},
-        //         {title: 'shazam!'}
-        //     ]
-        // })
+        axios.get('http://localhost:3000/api/books/all')
+        .then(response => {
+            res.render('admin/viewers/viewBooks', {
+                username: 'Arthur',
+                num: 3, 
+                title: 'alsdhjflsj',
+                books: response.data
+            });
+        })
+        .catch(err => console.log(err));
     }
 }
