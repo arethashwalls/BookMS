8c761a841c6e9c819ace906dd1338e32fed1da2b
ASH Walls <admin@arethawalls.com>
2019-04-04 15:16:03 -0700

    Got authors displaying properly

--- controllers/adminControllers/viewControllers/viewBookController.js
+++ controllers/adminControllers/viewControllers/viewBookController.js
@@ -1,14 +1,19 @@
 const axios = require('axios');
+const { authorfy } = require('../../../utils/formaters');
 
 module.exports = {
     getViewBooks: (req, res) => {
         axios.get('http://localhost:3000/api/books/all')
         .then(response => {
+            const { data: books } = response;
+            books.forEach(book => {
+                book.authors = authorfy(book.authors.map(author => author.name));
+            });
             res.render('admin/viewers/viewBooks', {
                 username: 'Arthur',
                 num: 3, 
                 title: 'alsdhjflsj',
-                books: response.data
+                books
             });
         })
         .catch(err => console.log(err));
--- controllers/apiControllers/bookController.js
+++ controllers/apiControllers/bookController.js
@@ -3,6 +3,7 @@ const { Book } = require('../../models');
 module.exports = {
     getAllBooks: (req, res) => {
         Book.find({})
+        .populate('authors')
         .then(books => res.json(books))
         .catch(err => console.log(err));
     }
