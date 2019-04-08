7455da73bd113b348044db64153e9ce80735f6ce
ASH Walls <admin@arethawalls.com>
2019-04-08 15:36:54 -0700

    Working on viewbook

--- controllers/adminControllers/viewControllers/viewOneControllers/viewBookController.js
+++ controllers/adminControllers/viewControllers/viewOneControllers/viewBookController.js
@@ -1,11 +1,16 @@
-const { Book } = require('../../../../models');
+const { Book } = require('../../../../models'),
+      { authorfy } = require('../../../../utils/formaters');
 
 module.exports = {
     viewBook: (req, res) => {
         const { url_title } = req.params;
         Book.findOne({url_title})
         .then(book => {
-            res.json(book)
+            res.render('admin/viewers/viewOne/viewBook', {
+                username: 'aaah',
+                authorstring: authorfy(book.authors),
+                book
+            })
         })
         .catch(err => console.log(err));
     }
