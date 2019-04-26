e64ea00cc9b6623b8354c52c803d59cb6dab0def
ASH Walls <admin@arethawalls.com>
2019-04-26 13:07:44 -0700

    More renaming

--- controllers/adminControllers/viewControllers/allControllers/bookSubControllers/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.readdirSync(__dirname).forEach(file => {
-    if(file !== 'index.js') {
-        const filename = file.replace('.js', '');
-        module.exports[filename] = require('./' + file);
-    }
-});
