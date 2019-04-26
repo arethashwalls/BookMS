0e8ccffc17093efafbd75913dad00f0d7ec2effc
ASH Walls <admin@arethawalls.com>
2019-04-25 19:49:27 -0700

    Getting new view route displaying properly

--- routes/adminRoutes/newRoutes/index.js
+++ routes/adminRoutes/newRoutes/index.js
@@ -1,12 +1,12 @@
 const express = require('express'),
       router = express.Router(),
       newBookRoutes = require('./newBookRoutes'),
-      newBookComponentRoutes = require('./newBookComponentRoutes'),
+      newBookChapterRoutes = require('./newBookChapterRoutes'),
       newAuthorRoutes = require('./newAuthorRoutes'),
       newPageRoutes = require('./newPageRoutes');
 
 router.use('/book', newBookRoutes);
-router.use('/:alias', newBookComponentRoutes);
+router.use('/chapter', newBookChapterRoutes);
 router.use('/author', newAuthorRoutes);
 router.use('/page', newPageRoutes);
 
--- routes/adminRoutes/newRoutes/newBookChapterRoutes.js
+++ routes/adminRoutes/newRoutes/newBookChapterRoutes.js
@@ -2,7 +2,7 @@ const express = require('express'),
       router = express.Router(),
       { newBookChapterController } = require('../../../controllers').adminControllers.newControllers;
 
-router.route('/chapter')
+router.route('/:alias')
     .get(newBookChapterController.getNewBookChapter);
 
 module.exports = router;
