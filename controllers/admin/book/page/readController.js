cb35d87386dfcf43176c007cfa8faec44f9472a2
ASH Walls <admin@arethawalls.com>
2019-04-26 14:46:35 -0700

    Added book/page controllers

---
+++ controllers/admin/book/page/readController.js
@@ -0,0 +1,35 @@
+const { Book, Page } = require('../../../../models'),
+     { truncate } = require('../../../../utils').formaters;
+
+module.exports = {
+    getPage: (req, res) => {
+        const { p_alias } = req.params;
+        Page.findOne({alias: p_alias})
+        .then(page => {
+            res.render('admin/viewers/one/page', {
+                username: 'aaah',
+                siteTitle: 'aah',
+                inAdmin: true,
+                page
+            });
+        })
+        .catch(err => console.log(err));
+    },
+    getPages: (req, res) => {
+        const { alias } = req.params;
+        Book.findOne({alias})
+        .populate('stdalone_pages')
+        .then(response => {
+            const { stdalone_pages, site_title, books } = response;
+            stdalone_pages.forEach(page => page.p_content = truncate(page.p_content));
+            res.render('admin/viewers/all/pages', {
+                username: 'Arthur',
+                inAdmin: true,
+                num: books.length,
+                siteTitle: site_title,
+                stdalonePages: stdalone_pages
+            });
+        })
+        .catch(err => console.log(err));
+    }
+}
