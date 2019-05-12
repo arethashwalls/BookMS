52ce3c4728f5b1a33b806982f1700f78e8bf8b0e
ASH Walls <admin@arethawalls.com>
2019-05-12 13:02:12 -0700

    Got rid of old controllers

--- controllers/apiControllers/userController.js
+++
@@ -1,9 +0,0 @@
-const { User } = require('../../models');
-
-module.exports = {
-    getSiteTitle: (req, res) => {
-        User.findOne({}, 'siteTitle')
-        .then(user => res.json(user.siteTitle))
-        .catch(err => console.log(err));
-    }
-}
